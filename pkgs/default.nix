e6a5e1dbb9e8c7a9347a7c629966a69673db602a
Pablo Hirafuji <pablohirafuji@gmail.com>
2024-12-27 23:50:06 -0300

    Update default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
-{pkgs}: rec {
+{pkgs, ...}: rec {
   linuxOrangePiZero3 = pkgs.callPackage ./linux {};
   ubootOrangePiZero3 = pkgs.callPackage ./uboot {};
   firmwareOrangePiZero3 = pkgs.callPackage ./firmware {};
